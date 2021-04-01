a7c3afeadb2385b5a3715dedd1ca2f0b5ff4bed9
rakeshrak34 <rakeshtummagunta9@gmail.com>
2021-04-01 10:09:25 -0700

    second one in ss

--- print.sh
+++ print.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo -e "hello\nworld"
-echo -e "\e[31mHello in Red"
-echo -e "\e[1;31mHello in bold red "
+echo -e "\e[31mHello in Red\e[0m"
+echo -e "\e[1;31mHello in bold red\e[0m"
 echo "+++++++++++++end++++++++++"
